d85d0600226bf02f4bef33ff97ac9e80a6905cfb
muffy <marufeuille@gmail.com>
2020-06-07 16:21:44 +0900

    modified

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,11 @@ git config --global push.default simple
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
+echo $CIRCLE_REPOSITORY_URL
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
-rsync -arv --delete ../doc/* .
+rsync -arv --delete ../docs/* .
 
 echo git add -f .
 echo git commit -m "Deploy build $CIRCLE_BUILD_NUM [ci skip]" || true
