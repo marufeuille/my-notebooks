e0c9f6837beda88ce6ed7644e0e95c3f3ac42c9c
muffy <marufeuille@gmail.com>
2020-06-07 16:30:41 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ git config --global user.name $CIRCLE_USERNAME
 git clone -q --branch=gh-pages $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 cd $DEPLOY_DIR
-rsync -arv --delete ../docs/* .
+rm -f *
+cp -r ../docs/* .
 echo $(ls)
 echo $(pwd)
 
